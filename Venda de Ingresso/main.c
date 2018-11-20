e2cc3500a43672365ce592b557881ffee4a8e3e5
Marcos Pivetta <marcospivetta.ti@gmail.com>
2018-11-20 08:08:47 -0200

    Initial Commit

---
+++ Venda de Ingresso/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Venda de Ingresso
+//
+//  Created by Marcos Pivetta on 20/11/18.
+//  Copyright © 2018 Marcos Pivetta. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
